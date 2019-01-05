cd8f4618b84a94c89191db71dc74b62c8eb8e84a
Douglas Lopes <dwillian.lopes@gmail.com>
2019-01-05 14:06:57 -0200

    auto scrolling

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,21 @@
 const socket = io();
 
+function scrollToBottom () {
+    //Selectors
+    let messages  = $('#messages');
+    let newMessage = messages.children('li:last-child');
+    // heights
+    let clientHeight = messages.prop('clientHeight');
+    let scrollTop = messages.prop('scrollTop');
+    let scrollHeight = messages.prop('scrollHeight');
+    let newMessageHeight = newMessage.innerHeight();
+    let lastMessageHeight = newMessage.prev().innerHeight();
+
+    if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+        messages.scrollTop(scrollHeight);
+    }
+}
+
 socket.on('connect', function () {
     console.log('Connected to server');
 });
@@ -18,6 +34,7 @@ socket.on('newMessage', function(message) {
     });
 
     $('#messages').append(html);
+    scrollToBottom();
 });
 
 socket.on('newLocationMessage', function({from, url, createdAt}) {
@@ -29,6 +46,7 @@ socket.on('newLocationMessage', function({from, url, createdAt}) {
         createdAt: formattedTime
     });
     $('#messages').append(html);
+    scrollToBottom();
 });
 
 
